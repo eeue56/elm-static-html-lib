3d9a563d1b840953e0b8f336048a08268e412358
eeue56 <enalicho@gmail.com>
2017-07-27 18:03:46 +0200

    improve example

--- example/index.ts
+++ example/index.ts
@@ -2,18 +2,20 @@ import elmStaticHtml from "elm-static-html-lib";
 import * as fs from "fs";
 
 const model = { name: "Noah", age : 24 };
+const secondModel = { name: "not noah", age: 74};
 const firstRunOptions = { model : model, decoder: "MyModule.decodeModel", alreadyRun: false };
-const secondRunOptions = { model : model, decoder: "MyModule.decodeModel", alreadyRun: true };
+const secondRunOptions = { model : secondModel, decoder: "MyModule.decodeModel", alreadyRun: true };
 
 
 function runTwice() {
     elmStaticHtml(process.cwd(), "MyModule.view", firstRunOptions)
     .then((generatedHtml) => {
         fs.writeFileSync("output.html", generatedHtml);
-    }).then(() => {
         elmStaticHtml(process.cwd(), "MyModule.view", secondRunOptions)
         .then((generatedHtml) => {
             fs.writeFileSync("output2.html", generatedHtml);
         });
     });
 }
+
+runTwice();
